3390b387738003613ae1241c104dbe476626d649
Sherzodbek1717 <wiut13457@gmail.com>
2023-04-19 13:25:31 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// console.log('salom');
+function salom(ism) {
+console.log('salom Sherzodbek');
+}
+
+salom();
+ 
+// don't repeat yourself means create function babe
+
+// function yaratirw ucun fucntion() {}
+
+// funksiya bn methodlar bir xil narsa
